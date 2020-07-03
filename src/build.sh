b8b34fe70038ac2792270d14124384f7f110b979
blackc8 <blackc8official@gmail.com>
2020-07-03 23:16:29 +0530

    cleaned build

--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-pkg install cmake clang
+pkg install clang termux-elf-cleaner
 name="termux-lock"
 vers="0.1"
 appN=$name"_"$vers"_arm"
 echo "build $name($vers)"
 echo "[+] building binaries..."
-cmake .
+gcc plogin.c -l termux-auth -o plogin
+echo "[+] Cleaning..."
+termux-elf-cleaner plogin
 echo "[+] creating package..."
 chmod 0775 -R $appN
 cp plogin $appN"/data/data/com.termux/files/usr/bin/plogin"
